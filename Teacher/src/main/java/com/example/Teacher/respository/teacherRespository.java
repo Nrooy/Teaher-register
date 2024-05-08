4f4050861bf78c55b662aa778f7df4fb7953b339
leminhat <nhat0886519399@gmail.com>
2024-05-08 11:29:58 +0700

    nhat unittest

--- Teacher/src/main/java/com/example/Teacher/respository/teacherRespository.java
+++ Teacher/src/main/java/com/example/Teacher/respository/teacherRespository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface teacherRespository extends JpaRepository<Teacher,Integer> {
     @Query(value = "select t from Teacher t where t.id = :idStaff")
     Teacher findTeacher(int idStaff);
-    @Query(value = "select t from Teacher t where t.id = :idDepartment")
-    List<Teacher> findAllByIdDepartment(int idDepartment);
+//    @Query(value = "select t from Teacher t where t.id = :idDepartment")
+//    List<Teacher> findAllByIdDepartment(int idDepartment);
 }
